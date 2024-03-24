c63eed1a9436612ce5a472acfd7b9e3bc8d76d6e
Matthew <matthew.aucott@datanova.com.au>
2024-03-24 14:20:27 +1000

    Refactor itemTester method in ItemTester class

--- components/itemTester.py
+++ components/itemTester.py
@@ -1,33 +1,35 @@
 import requests
-import json
 import concurrent.futures
 from components.config import Config
 
 
 class ItemTester:
 
-	def itemTester(item1, item2):
-		with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
-			future = executor.submit(ItemTester.testItem, item1, item2)
+	@classmethod
+	def itemTester(cls, item1, item2):
+		with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+			future = executor.submit(cls.testItem, item1, item2)
 			return future.result()
 
-
+	@staticmethod
 	def testItem(item1, item2):
 		params = {
 			'first': item1,
 			'second': item2,
 		}
 
-		while True:
-			try:
-				response = requests.get('https://neal.fun/api/infinite-craft/pair', params=params, cookies=Config.cookies, headers=Config.headers)
-				response = json.loads(response.text)
-				break
-			except Exception as e:
-				print(e)
+		with requests.Session() as session:
+			while True:
+				try:
+					response = session.get('https://neal.fun/api/infinite-craft/pair', params=params, cookies=Config.cookies, headers=Config.headers, timeout=5)
+					response.raise_for_status()
+					data = response.json()
+					break
+				except Exception as e:
+					print(e)
 
 		return {
-			'result': response['result'],
-			'emoji': response['emoji'],
-			'isNew': response['isNew']
+			'result': data['result'],
+			'emoji': data['emoji'],
+			'isNew': data['isNew']
 		}
