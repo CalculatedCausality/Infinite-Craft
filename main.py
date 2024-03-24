cfa3cecc6ddf8ee31c78f4bd2673795f70286858
Matthew <matthew.aucott@datanova.com.au>
2024-03-24 14:08:49 +1000

    Fix SQL syntax error in main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
 		with Database.db_lock:
 			conn = Database.getDbConnections()
 			c = conn.cursor()
-			c.execute("SELECT item FROM items WHERE item NOT IN (?, ?)", ('?', '???'))
+			c.execute("SELECT item FROM items"))
 			discovered_items = set(item[0] for item in c.fetchall())
 
 		BruteForce.brute_force(discovered_items)
